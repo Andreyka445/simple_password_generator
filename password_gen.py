2aeda53b41c1638b9cc5554320f1c0abb6150c89
Andreyka445 <seleznevandrey186@outlook.com>
2025-08-28 03:09:18 +0700

    Update password_gen.py
    simple edit

--- password_gen.py
+++ password_gen.py
@@ -3,9 +3,7 @@ import string
 
 # длина пароля
 length = 12
-
 # Символы
-
 characters = "abcdefghijkmnoprstuvwxyzABCDEFGHIJKMNOPRSTUWXYZ123456789"
 # Генерим пароль
 password = ''.join(random.choice(string.asci+ string.digits) for _ in range(length))
