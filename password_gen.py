3511e16b5e323af4f8e0672072c72ce0db99afa9
Andreyka445 <seleznevandrey186@outlook.com>
2025-08-28 03:03:19 +0700

    Update password_gen.py
    edit chars

--- password_gen.py
+++ password_gen.py
@@ -4,4 +4,4 @@ import string
 # длина пароля
 length = 12
 
-# characters = "abcdefghijkmnoprstuvwxyzABCDEFGHIJKMNOPRSTUWXYZ"XYZZ890
+# characters = "abcdefghijkmnoprstuvwxyzABCDEFGHIJKMNOPRSTUWXYZ123456789"
