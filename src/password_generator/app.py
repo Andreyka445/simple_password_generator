04390c6106ef121bc66b540511ebf7abaccda56d
Andreyka445 <seleznevandrey186@outlook.com>
2025-08-28 20:57:38 +0700

    Update app.py
    import toga 
    and create conatiner

--- src/password_generator/app.py
+++ src/password_generator/app.py
@@ -1,12 +1,16 @@
+import toga
+from toga.style import Pack
+from toga.tyle.pack import COLUMN, ROW
 import random
 import string
-
+# оригинальная конструкция кода
 def generate_password(length=12):
     characters = string.ascii_letters + string.digits
     password = ''.join(random.choice(characters) for _ in range(length))
     return password
 
-# генерим пароль
-if __name__ == "__main__":
-    generated_password = generate_password()
-    print(f"пасс гее: {generated_password}")
+class PasswordGenerator(toga.App):
+    def startup(self):
+        # главный контейнер
+        main_box = toga.Box(style=Pack(direction=COLUMN, padding=10))
+        
