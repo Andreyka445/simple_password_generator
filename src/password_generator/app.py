25c13a209ca138f6ed0e5582c214d334f501c4f0
Andreyka445 <seleznevandrey186@outlook.com>
2025-08-28 21:04:43 +0700

    Update app.py
    адд заголовк 
    конейнер для выбоора длинны
    поле для ввода доины пароя
    поле для вывода пароля

--- src/password_generator/app.py
+++ src/password_generator/app.py
@@ -13,4 +13,29 @@ class PasswordGenerator(toga.App):
     def startup(self):
         # главный контейнер
         main_box = toga.Box(style=Pack(direction=COLUMN, padding=10))
+
+# Zagalovok
+ title_label = toga.Label(
+            'Генератор паролей', 
+            style=Pack(font_size=20, padding_bottom=10)
+        )
+# конетинер для выбора длинны 
+length_box = toga.Box(style=Pack(direction=ROW, padding=5))
+        length_label = toga.Label('длина пароль:', style=Pack(padding_right=5))
+ # поле для ввода доины
+        self.length_input = toga.NumberInput(
+            min_value=4, 
+            max_value=32, 
+            value=12,
+            style=Pack(width=100)
+        )
+ # выводим пароль
+        self.password_input = toga.TextInput(
+            placeholder='пароль:', 
+            readonly=True,
+            style=Pack(padding=5, flex=1)
+        )
+ # контейнер для ионокфв   
+
+
         
